75716cb03170c0a87b1c6b98af5f1720ba4afab6
Igorshk10 <igorshk271@gmail.com>
2025-03-29 14:00:58 +0200

    update UserService

--- routes/main.js
+++ routes/main.js
@@ -6,20 +6,11 @@ var UserService = require('../services/userService');
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
     const user_username = req.session.user.username;
- //   const userId = req.session.user.id;
- //   const transaction = await UserService.getTransactionById(userId);
+    const userId = req.session.user.id;
+    const transaction = await UserService.getTransactionById(userId);
     res.render('main', { username: user_username});
 });
 
-router.get('/api/transactions', async function(req, res) {
-    const userId = req.session.user.id;
-    try {
-        const transactions = await UserService.getTransactionById(userId);
-        res.json(transactions);
-    } catch (error) {
-        res.status(500).json({ error: 'Не вдалося отримати транзакції' });
-    }
-});
 
 router.post('/transaction', async function(req, res, next) {
     const userId = req.session.user.id;
